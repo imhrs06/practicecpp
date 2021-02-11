591251c50a499e862e589da4e610d191c755a706
Harsh kumar <71516936+imhrs06@users.noreply.github.com>
2021-02-12 04:54:20 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//4.write a progrm to find input number is even or odd.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the number:";
+	cin>>n;
+	if(n%2==0)
+	{
+		cout<<n<<" is even number";
+	}
+	else
+	{
+		cout<<n<<" is odd number";
+	}
+	
+}
